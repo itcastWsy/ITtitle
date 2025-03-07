93a6a00ec410a50de95cbfee658e823fa5a208ca
uksir <yeah126139163@163.com>
2025-03-07 09:47:41 +0800

    完成登录页面的设计

--- src/layout/Layout.vue
+++ src/layout/Layout.vue
@@ -1,5 +1,3 @@
-<script setup lang="ts"></script>
-
 <template>
   <div class="layout-page">
     <div class="layout-top">
@@ -18,12 +16,38 @@
     </div>
     <div class="layout-main">
       <div class="layout-left">
-        <el-button>Default</el-button>
-        <el-button type="primary">Primary</el-button>
-        <el-button type="success">Success</el-button>
-        <el-button type="info">Info</el-button>
-        <el-button type="warning">Warning</el-button>
-        <el-button type="danger">Danger</el-button>
+        <el-menu default-active="2">
+          <el-sub-menu index="1">
+            <template #title>
+              <el-icon><location /></el-icon>
+              <span>Navigator One</span>
+            </template>
+            <el-menu-item-group>
+              <template #title><span>Group One</span></template>
+              <el-menu-item index="1-1">item one</el-menu-item>
+              <el-menu-item index="1-2">item two</el-menu-item>
+            </el-menu-item-group>
+            <el-menu-item-group title="Group Two">
+              <el-menu-item index="1-3">item three</el-menu-item>
+            </el-menu-item-group>
+            <el-sub-menu index="1-4">
+              <template #title><span>item four</span></template>
+              <el-menu-item index="1-4-1">item one</el-menu-item>
+            </el-sub-menu>
+          </el-sub-menu>
+          <el-menu-item index="2">
+            <el-icon><icon-menu /></el-icon>
+            <template #title>Navigator Two</template>
+          </el-menu-item>
+          <el-menu-item index="3" disabled>
+            <el-icon><document /></el-icon>
+            <template #title>Navigator Three</template>
+          </el-menu-item>
+          <el-menu-item index="4">
+            <el-icon><setting /></el-icon>
+            <template #title>Navigator Four</template>
+          </el-menu-item>
+        </el-menu>
       </div>
       <div class="layout-right">
         <router-view />
@@ -31,6 +55,10 @@
     </div>
   </div>
 </template>
+
+<script setup lang="ts">
+import { Document, Menu as IconMenu, Location, Setting } from '@element-plus/icons-vue'
+</script>
 
 <style lang="scss" scoped>
 .layout-page {
@@ -42,6 +70,14 @@
   height: 100px;
   background-color: #00569a;
   padding: 10px;
+}
+.layout-main {
+  display: flex;
+  height: calc(100vh - 100px);
+}
+.layout-left {
+  width: 200px;
+  background-color: #004dd7;
 }
 .title {
   color: #fff;
--- src/views/login/Login.vue
+++ src/views/login/Login.vue
@@ -1,7 +1,58 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="login-page">login</div>
+  <div class="login-page">
+    <div class="login-main">
+      <div class="title">黑马头条后台管理系统</div>
+      <el-form label-width="80px" class="login-form">
+        <el-form-item label="手机号码">
+          <el-input></el-input>
+        </el-form-item>
+        <el-form-item label="验证码">
+          <el-input>
+            <template v-slot:append>
+              <span class="code-btn">获取验证码</span>
+            </template>
+          </el-input>
+        </el-form-item>
+
+        <el-form-item>
+          <el-button type="primary">登录</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.login-page {
+  width: 100vw;
+  height: 100vh;
+  background-color: #2f6dff;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+.login-main {
+  border-radius: 5px;
+  background-color: #fff;
+  padding: 0 50px;
+}
+.title {
+  padding: 20px;
+  font-size: 30px;
+  color: #409eff;
+}
+.login-form {
+  background-color: #fff;
+  padding: 40px;
+  border-radius: 5px;
+}
+.code-btn {
+  cursor: pointer;
+  color: #409eff;
+  font-size: 14px;
+  white-space: nowrap;
+}
+</style>
