737b4eb2099531e9ef366bd150676ab666b5e916
uksir <yeah126139163@163.com>
2025-03-07 18:33:11 +0800

    添加了持久化

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -3,12 +3,18 @@ import { defineStore } from 'pinia'
 import { login } from '@/services/user'
 import type { LoginParams } from '@/types'
 
-export const useUserStore = defineStore('user', () => {
-  const token = ref('')
-  const fetchToken = async (data: LoginParams) => {
-    const result = await login(data)
-    token.value = result.data.token
-    console.log(token.value)
-  }
-  return { token, fetchToken }
-})
+export const useUserStore = defineStore(
+  'user',
+  () => {
+    const token = ref('')
+    const fetchToken = async (data: LoginParams) => {
+      const result = await login(data)
+      token.value = result.data.token
+      console.log(token.value)
+    }
+    return { token, fetchToken }
+  },
+  {
+    persist: true,
+  },
+)
