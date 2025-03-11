1e1451bad38bfd1e1ecabf60f5c732928a55370d
uksir <yeah126139163@163.com>
2025-03-11 16:28:57 +0800

    3333

--- vite.config.ts
+++ vite.config.ts
@@ -24,5 +24,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-  base: '.',
+  base: process.env.NODE_ENV === 'production' ? '/ITtitle/' : '/',
 })
